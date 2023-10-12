8dc5a5f9d38f26e75d6dccb5853bff94811fc597
Scott Hallock <scott.hallock@gmail.com>
2023-10-11 22:32:09 -0400

    solve daily-temperatures

--- daily-temperatures.js
+++ daily-temperatures.js
@@ -19,21 +19,49 @@ Output: [1,1,0]
 /*
 Brute-force solution
 */
+// const dailyTemperatures = (temps) => {
+//   const result = [];
+//   // Iterate through each day
+//   for (let i = 0; i < temps.length; i++) {
+//     // Iterate through each day after the initial day
+//     let j = i + 1;
+//     let waitTime = 1;
+//     // Look for a warmer day or reach the end of the array
+//     while (temps[i] >= temps[j] && j < temps.length) {
+//       waitTime++;
+//       j++;
+//     }
+//     result.push(j === temps.length ? 0 : waitTime);
+//   }
+//   return result;
+// };
+
 const dailyTemperatures = (temps) => {
-  const result = [];
-  // Iterate through each day
-  for (let i = 0; i < temps.length; i++) {
-    // Iterate through each day after the initial day
-    let j = i + 1;
-    let waitTime = 1;
-    // Look for a warmer day or reach the end of the array
-    while (temps[i] >= temps[j] && j < temps.length) {
-      waitTime++;
-      j++;
+  // Create an n-sized array filled with 0s
+  const answer = Array(temps.length).fill(0);
+
+  // Create a stack that will hold days
+  // such that the temperatures are in decreasing order
+  const stack = [];
+
+  for (let currDay = 0; currDay < temps.length; currDay++) {
+    const currTemp = temps[currDay];
+
+    while (stack.length && temps[stack.at(-1)] < currTemp) {
+      // The previous day (on the stack) temperature is less than
+      // the current temperature
+      //
+      // This means we have an answer for the previous day!
+      // The wait time for prevDay is equal to currDay - prevDay
+      const prevDay = stack.pop();
+      answer[prevDay] = currDay - prevDay;
     }
-    result.push(j === temps.length ? 0 : waitTime);
+
+    // Add the current day on the stack
+    stack.push(currDay);
   }
-  return result;
+
+  return answer;
 };
 
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
